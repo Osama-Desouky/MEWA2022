241cc2099e513579a360f2f2fdb2390abad84319
elomar1 <ahmedomarabdallah1@gmail.com>
2022-06-23 12:58:31 +0200

    adding home page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,7 +2,7 @@
 
 document.addEventListener("DOMContentLoaded",function(){
     window.addEventListener("scroll",function() {
-       console.log(window.scrollY,window.scrollX)
+    //    console.log(window.scrollY,window.scrollX)
        if(window.scrollY > 50) {
         document.querySelector('.menuBg').classList.add('fixedTop')
                 // add padding top to show content behind navbar
@@ -17,6 +17,159 @@ document.addEventListener("DOMContentLoaded",function(){
 
 
 
+
 })
 
 
+
+$(document).ready(function(){
+
+$("a").click(function( e ) {
+    e.preventDefault();
+
+    if(this.hash){
+
+        var target = '#'+ this.hash.substr(1);
+        var destination = $(target).offset().top - 10;
+
+        $('#main').stop().animate({
+            scrollTop : '+=' + destination
+        }, 1000);
+    }
+});
+
+    $('.scrollTop').on('click', function() {
+      $('html, body').animate({
+        scrollTop: $('html, body').offset().top,
+      });
+    });
+
+    $('.HappensNowOpen').click(function(){
+
+        $(".happeningNow").toggleClass('openUpSection')
+    })
+    $('.owl-carousel-header').owlCarousel({
+      loop:false,
+      rtl: true,
+      margin:10,
+      nav:true,
+      navText : ['<img src="../assets/images/home/arrowOwlH2.svg" />','<img src="../assets/images/home/arrowOwlH1.svg" />'],
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:1
+          }
+      }
+  })
+
+  $('.owl-carousel-center').owlCarousel({
+    loop:false,
+    margin:35,
+    nav:true,
+    navText : ['<img src="../assets/images/home/arrowSlider2.svg" />','<img src="../assets/images/home/arrowSlider1.svg" />'],
+    rtl: true,
+    stagePadding: 38,
+    // responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+   
+        },
+        600:{
+            items:1,
+
+        },
+        1000:{
+            items:3,
+ 
+        }
+    }
+})
+
+  $('.owl-carousel-news').owlCarousel({
+    loop:false,
+    margin:40,
+    rtl: true,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        1000:{
+            items:2
+        },
+        1400:{
+            items:3
+        }
+    }
+})
+$('.owl-carousel-event1').owlCarousel({
+    loop:false,
+    margin:40,
+    stagePadding: 30,
+    rtl: true,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        1000:{
+            items:2
+        },
+        1400:{
+            items:3
+        }
+    }
+})
+$('.owl-carousel-event2').owlCarousel({
+    loop:true,
+    margin:40,
+    stagePadding: 25,
+    rtl: true,
+    nav:true,
+    navText : ['<img src="../assets/images/home/arrowSlider1.svg" />','<img src="../assets/images/home/arrowSlider2.svg" />'],
+    // items:3.5,
+    responsive:{
+        0:{
+            items:1
+        },
+        1000:{
+            items:2.5
+        },
+        1500:{
+            items:3.5
+        }
+    }
+})
+  const swiper = new Swiper('.swiperMain', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: false,
+    // autoplay: {
+    //     delay: 5000,
+    //   },
+    // If we need pagination
+    pagination: {
+    //   el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      // el: '.swiper-scrollbar',
+    },
+  });
+
+
+
+  });
